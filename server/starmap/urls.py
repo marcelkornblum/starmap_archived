0b7bf27d7dce9db32cfb44452c2d71fd3da1eca1
marcelkornblum <me@marcelkornblum.com>
2015-04-29 19:25:36 +0100

    update

--- server/starmap/urls.py
+++ server/starmap/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'starmap.views.home', name='home'),
+    url(r'^$', 'starmap.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^stars/', include('stardata.urls')),
     url(r'^admin/', include(admin.site.urls)),
